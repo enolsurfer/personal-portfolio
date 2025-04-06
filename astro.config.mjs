15a59fc5fcc81df54da581462fa11edb453104ec
enolsurfer <enolgs99@gmail.com>
2025-04-06 17:29:57 +0200

    Configura astro.config.mjs para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://<enolsurfer>.github.io/<personal-portfolio>',
+  base: '/<personal-portfolio>/', 
+  output: 'static',
+});
